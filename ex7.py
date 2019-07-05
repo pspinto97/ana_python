a9c9500deb59e800caa21008a3bec3d642944ece
pspinto97 <52583962+pspinto97@users.noreply.github.com>
2019-07-05 22:37:54 +0100

    Create ex7.py

---
+++ ex7.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+
+
+from math import sqrt
+
+x1 = float(input("x1? "))
+y1 = float(input("y1? "))
+x2 = float(input("x2? "))
+y2 = float(input("y2? "))
+
+d = sqrt((x2-x1)**2 + (y2-y1)**2)
+
+print ()
+print ("A distância entre o ponto(" + str(x1) + ",", str(y1) + ") e o ponto (" + str(x2) + ",",$
+print ()
