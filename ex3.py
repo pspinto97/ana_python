c9e34d45232953790b0bd5578131662f9a32876a
pspinto97 <52583962+pspinto97@users.noreply.github.com>
2019-07-05 22:23:47 +0100

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+
+
+nome = input("Como te chamas? ")
+ano = int(input("Em que ano nasceste? "))
+
+novo = 2020-ano
+
+print (nome + ", em 2020 farás", novo, "anos.")
