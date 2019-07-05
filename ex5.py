def32f43a53b12f2982f632cd1e7a2591351693a
pspinto97 <52583962+pspinto97@users.noreply.github.com>
2019-07-05 22:27:23 +0100

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+
+
+def calcula_km():
+    """
+    Total dos 3 andares = 9m
+    Andar 1 = 3, Andar = 2, Andar = 3
+    1 morador por andar * 2 utilização
+    Devolve o resultado em metros
+    """
+
+    # Andar(n) = Número * metros * moradores * utilizações
+    andar1 = 1*3*1*2
+    andar2 = 2*3*1*2
+    andar3 = 3*3*1*2
+
+    return (andar1 + andar2 + andar3) * 365
+
+
+
+def calcula_tempo(temp):
+    """
+    Recebe como parâmetro o resultado da função calcula_km
+    Tempo = distância / velocidade
+    Devolve o tempo em segundos
+    """
+
+    return temp/1
+
+km = calcula_km()/1000
+h, m = divmod(calcula_tempo(calcula_km()), 60)
+
+print ()
+print ("Num ano o elevador percorre", str(km) + "km.")
+print ("Num ano o elevador trabalha durante", str(h) + "H", str(m) + "M")
+print()
